696c01f2ba9ebb029986b74894b8b83e33169b40
badi3a <badiaabouhdid@gmail.com>
2022-01-08 16:37:57 +0100

    first WS:Hello

--- src/main/java/endpoint/RestActivator.java
+++ src/main/java/endpoint/RestActivator.java
@@ -1,6 +1,10 @@
 package endpoint;
 
-public class RestActivator {
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("rest")
+public class RestActivator extends Application{
 
 	public RestActivator() {
 		// TODO Auto-generated constructor stub
--- src/main/java/ressources/Hello.java
+++ src/main/java/ressources/Hello.java
@@ -1,9 +1,24 @@
 package ressources;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.*;
+
+@Path("greeting")
 public class Hello {
 
 	public Hello() {
 		// TODO Auto-generated constructor stub
 	}
+    @Path("hello")
+    @GET
+    @Produces(MediaType.TEXT_PLAIN) //le type de retour
+	public Response hello() {
+		return Response
+				.status(200)
+				.entity("hello :)")
+				.build()
+;	}
 
 }
